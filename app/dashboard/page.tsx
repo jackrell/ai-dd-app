4ec7fe622881b09174f995680021ab63b89adb7a
Jack Rellinger <jackrellinger@Jacks-MacBook-Pro-3.local>
2024-06-17 14:14:13 -0400

    Dashboard rough version running

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,24 +4,25 @@ import prisma from '@/utils/prisma';
 // import type { User } from '@clerk/nextjs/api';
 
 export default async function Page() {
-  const user: User | null = await currentUser();
+  // const user: User | null = await currentUser();
 
   // Fetch the list of folders for the current user
-  const foldersList = await prisma.document.groupBy({
-    by: ['namespace'],
-    // where: {
-    //   userId: user?.id,
-    // },
-    // _min: {
-    //   createdAt: true,
-    // },
-    // orderBy: {
-    //   _min: {
-    //     createdAt: 'desc',
-    //   },
-    // },
-  });
+//   const foldersList = await prisma.document.groupBy({
+//     by: ['namespace'],
+//     // where: {
+//     //   userId: user?.id,
+//     // },
+//     // _min: {
+//     //   createdAt: true,
+//     // },
+//     // orderBy: {
+//     //   _min: {
+//     //     createdAt: 'desc',
+//     //   },
+//     // },
+//   });
 
+ const foldersList = '';
   return (
     <div>
       <DashboardClient foldersList={foldersList} />
