b0fde6184a7001e8d61782f39ace27020b8bf72a
Jack Rellinger <jackrellinger@Jacks-MacBook-Pro-3.local>
2024-06-17 13:55:16 -0400

    finishing first dashboard build

---
+++ app/api/chat/route.ts
@@ -0,0 +1,92 @@
+import { NextRequest, NextResponse } from 'next/server';
+import type { Message as VercelChatMessage } from 'ai';
+import { createRAGChain } from '@/utils/ragChain';
+import type { Document } from '@langchain/core/documents';
+import { HumanMessage, AIMessage, ChatMessage } from '@langchain/core/messages';
+import { ChatTogetherAI } from '@langchain/community/chat_models/togetherai';
+import { loadRetriever } from '../utils/vector_store';
+import { loadEmbeddingsModel } from '../utils/embeddings';
+
+// need to alter further so that it works with the frontend (folderName passed as namespace)
+
+export const runtime = 'edge';
+
+const formatVercelMessages = (message: VercelChatMessage) => {
+  if (message.role === 'user') {
+    return new HumanMessage(message.content);
+  } else if (message.role === 'assistant') {
+    return new AIMessage(message.content);
+  } else {
+    console.warn(`Unknown message type passed: "${message.role}". Falling back to generic message type.`);
+    return new ChatMessage({ content: message.content, role: message.role });
+  }
+};
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const messages = body.messages ?? [];
+    if (!messages.length) {
+      throw new Error('No messages provided.');
+    }
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatVercelMessages);
+    const currentMessageContent = messages[messages.length - 1].content;
+    const folderName = body.folderName;
+
+    const model = new ChatTogetherAI({
+      modelName: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+      temperature: 0,
+    });
+
+    const embeddings = loadEmbeddingsModel();
+
+    let resolveWithDocuments: (value: Document[]) => void;
+    const documentPromise = new Promise<Document[]>((resolve) => {
+      resolveWithDocuments = resolve;
+    });
+
+    const retrieverInfo = await loadRetriever({
+      namespace: folderName,
+      embeddings,
+      callbacks: [
+        {
+          handleRetrieverEnd(documents) {
+            resolveWithDocuments(documents);
+          },
+        },
+      ],
+    });
+
+    const retriever = retrieverInfo.retriever;
+
+    const ragChain = await createRAGChain(model, retriever);
+
+    const stream = await ragChain.stream({
+      input: currentMessageContent,
+      chat_history: formattedPreviousMessages,
+    });
+
+    const documents = await documentPromise;
+    const serializedSources = Buffer.from(
+      JSON.stringify(
+        documents.map((doc) => {
+          return {
+            pageContent: doc.pageContent.slice(0, 50) + '...',
+            metadata: doc.metadata,
+          };
+        }),
+      ),
+    ).toString('base64');
+
+    const byteStream = stream.pipeThrough(new TextEncoderStream());
+
+    return new Response(byteStream, {
+      headers: {
+        'x-message-index': (formattedPreviousMessages.length + 1).toString(),
+        'x-sources': serializedSources,
+      },
+    });
+  } catch (e: any) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import DashboardClient from './dashboard-client';
+import prisma from '@/utils/prisma';
+// import { currentUser } from '@clerk/nextjs';
+// import type { User } from '@clerk/nextjs/api';
+
+export default async function Page() {
+  const user: User | null = await currentUser();
+
+  // Fetch the list of folders for the current user
+  const foldersList = await prisma.document.groupBy({
+    by: ['namespace'],
+    // where: {
+    //   userId: user?.id,
+    // },
+    // _min: {
+    //   createdAt: true,
+    // },
+    // orderBy: {
+    //   _min: {
+    //     createdAt: 'desc',
+    //   },
+    // },
+  });
+
+  return (
+    <div>
+      <DashboardClient foldersList={foldersList} />
+    </div>
+  );
+}
