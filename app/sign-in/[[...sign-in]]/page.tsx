868de2d05c0f178b658f658ab6a93137ae1e591a
Jack Rellinger <jackrellinger@Jacks-MacBook-Pro-3.local>
2024-06-17 16:21:20 -0400

    Added sign-in capabilities

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,20 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+    return (
+        // <div className="mt-5">
+        //   <Header />
+          <div className="flex mx-auto justify-center items-center mt-10">
+            <SignIn
+              appearance={{
+                elements: {
+                  formButtonPrimary:
+                    'bg-black hover:bg-gray-700 transition text-sm normal-case',
+                },
+              }}
+              afterSignUpUrl="/dashboard"
+            />
+          </div>
+        // </div>
+      );
+} 
