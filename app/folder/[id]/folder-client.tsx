00e5876eea10dce4d0f7b675da220a7fc218f918
Jack Rellinger <jackrellinger@Jacks-MacBook-Pro-3.local>
2024-07-08 15:37:47 -0400

    trying again

--- app/folder/[id]/folder-client.tsx
+++ app/folder/[id]/folder-client.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { React, useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import LoadingDots from '@/components/ui/LoadingDots';
